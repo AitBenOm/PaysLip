478c744c778e38c56733631fe0ca7b6e85ab0550
AitBenOm <omar.benaissa@outlook.com>
2018-05-14 10:58:45 +0200

    Add New Employee Form Validation

--- src/app/employee/add-employee/add-employee.component.ts
+++ src/app/employee/add-employee/add-employee.component.ts
@@ -25,46 +25,47 @@ export class AddEmployeeComponent implements OnInit {
     this.listFonction = this.employeeService.getListFunction();
     this.employeeForm = this.formBuilder.group(
       {
-        'nom': new FormControl(null, Validators.required),
-        'sex': new FormControl(null, Validators.required),
-        'prenom': new FormControl(null, Validators.required),
-        'date_de_naissance': new FormControl(null, [Validators.required, this.dateValidator.bind(this)]),
-        'situationFamiliale': new FormControl(null, Validators.required),
-        'nbEnfant': new FormControl(null, Validators.required),
-        'adresse': new FormControl(null, Validators.required),
-        'telephone': new FormControl(null, Validators.required),
-        'email': new FormControl(null, Validators.required),
-        'numCin': new FormControl(null, Validators.required),
-        'numCNSS': new FormControl(null, Validators.required),
-        'date_emb': new FormControl(null, [Validators.required, this.dateValidator.bind(this)]),
-        'fonction': new FormControl(null, Validators.required),
-        'salaireDeBase': new FormControl(null, [Validators.required, Validators.min(10)]),
+        nom: new FormControl(null, Validators.required),
+        sex: new FormControl(null, Validators.required),
+        prenom: new FormControl(null, Validators.required),
+        date_de_naissance: new FormControl(null, [Validators.required, this.dateValidator.bind(this)]),
+        situationFamiliale: new FormControl(null, Validators.required),
+        nbEnfant: new FormControl(null, Validators.required),
+        adresse: new FormControl(null, Validators.required),
+        telephone: new FormControl(null, Validators.required),
+        email: new FormControl(null, Validators.required),
+        numCin: new FormControl(null, Validators.required),
+        numCNSS: new FormControl(null, Validators.required),
+        date_emb: new FormControl(null, [Validators.required, this.dateValidator.bind(this)]),
+        fonction: new FormControl(null, Validators.required),
+        salaireDeBase: new FormControl(null, [Validators.required, Validators.min(10)]),
       }
     );
+    console.log(this.employeeForm.controls.nom);
   }
 
   dateValidator(date: FormControl) {
     const toDay = new Date();
     console.log(toDay);
     console.log(new Date(date.value));
-    console.log(new Date(date.value) > toDay);
-    if (      new Date(date.value) > toDay  ) {
-      if(new Date(date.value).getFullYear()<1900){
-        return {'dateNoOk': true};
-      }
-    }  return null;
+    console.log(new Date(date.value).getTime() > toDay.getTime());
+    if (new Date(date.value).getTime() > toDay.getTime() || new Date(date.value).getFullYear() < 1900) {
+
+      return {'dateNoOk': true};
+    }
+    return null;
   }
 
   addEmployee() {
-    console.log(this.employeeForm);
+    //  console.log(this.employeeForm);
     const matricule = this.employeeService.getListEmployee().length + 1;
 
-    // this.employeeService.onAddEmployee(
-    //   new EmployeeModel(matricule, employeeForm.value.nom, employeeForm.value.prenom, new Date(employeeForm.value.date_de_naissance), new Date(employeeForm.value.date_emb),
-    //     employeeForm.value.fonction, employeeForm.value.adresse, employeeForm.value.telephone, employeeForm.value.email, employeeForm.value.numCNSS,
-    //     employeeForm.value.numCin, employeeForm.value.sex, employeeForm.value.situationFamiliale, employeeForm.value.nbEnfant, employeeForm.value.salaireDeBase)
-    // );
-    // this.employeeService.onEmployeeAdded.next(this.employee);
+    this.employeeService.onAddEmployee(
+      new EmployeeModel(matricule, this.employeeForm.controls.nom.value, this.employeeForm.controls.prenom.value, new Date(this.employeeForm.controls.date_de_naissance.value), new Date(this.employeeForm.controls.date_emb.value),
+        this.employeeForm.controls.fonction.value, this.employeeForm.controls.adresse.value, this.employeeForm.controls.telephone.value, this.employeeForm.controls.email.value, this.employeeForm.controls.numCNSS.value,
+        this.employeeForm.controls.numCin.value, this.employeeForm.controls.sex.value, this.employeeForm.controls.situationFamiliale.value, this.employeeForm.controls.nbEnfant.value, this.employeeForm.controls.salaireDeBase.value)
+    );
+    //this.employeeService.onEmployeeAdded.next(this.employee);
     this.router.navigate(['employee-list']);
   }
 
